bc718158b84badd04c20256743e79456b6cca3c1
Klajd Budlla <klajdbudlla@Klajds-MacBook-Pro.local>
2019-05-07 14:54:40 +0200

    Updates

---
+++ precache-manifest.e26401da37fbf0f6d60df5f319d39258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3915dbd23905e37f0ded09c35c9eb7d3",
+    "url": "/deploy-lastfmclone/index.html"
+  },
+  {
+    "revision": "fceaab3d3657b35858f2",
+    "url": "/deploy-lastfmclone/static/css/2.49c70f4b.chunk.css"
+  },
+  {
+    "revision": "a262e740dd1a1c24e231",
+    "url": "/deploy-lastfmclone/static/css/main.0dd583d5.chunk.css"
+  },
+  {
+    "revision": "fceaab3d3657b35858f2",
+    "url": "/deploy-lastfmclone/static/js/2.0958a282.chunk.js"
+  },
+  {
+    "revision": "a262e740dd1a1c24e231",
+    "url": "/deploy-lastfmclone/static/js/main.91b9ef73.chunk.js"
+  },
+  {
+    "revision": "89492b4baf53abe3c8d0",
+    "url": "/deploy-lastfmclone/static/js/runtime~main.94dfe1a7.js"
+  }
+]);
