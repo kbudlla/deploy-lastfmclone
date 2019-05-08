9eed2a4a811056e7eeb07991d1ba2fd159402ee7
Klajd Budlla <klajdbudlla@Klajds-MacBook-Pro.local>
2019-05-08 11:50:05 +0200

    Updates

--- precache-manifest.043e5e12284cb3f20d8e3fd9857217e1.js
+++ precache-manifest.043e5e12284cb3f20d8e3fd9857217e1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8eaeae45772b8cfc93b863022069f257",
+    "revision": "cfb2d8a3bf24696b5999b4d6e3588e97",
     "url": "/deploy-lastfmclone/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/deploy-lastfmclone/static/css/2.266952b4.chunk.css"
   },
   {
-    "revision": "fa1d2b1c9f9eb96bfb2f",
+    "revision": "82b2bfdba2f30715ee1e",
     "url": "/deploy-lastfmclone/static/css/main.e64db05f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/deploy-lastfmclone/static/js/2.5b5d0236.chunk.js"
   },
   {
-    "revision": "fa1d2b1c9f9eb96bfb2f",
-    "url": "/deploy-lastfmclone/static/js/main.f06d5971.chunk.js"
+    "revision": "82b2bfdba2f30715ee1e",
+    "url": "/deploy-lastfmclone/static/js/main.8d3bd81e.chunk.js"
   },
   {
     "revision": "89492b4baf53abe3c8d0",
