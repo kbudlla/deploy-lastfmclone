eab8bedb5a2ce490bd7cf3d4d95e156aad4d90a1
Klajd Budlla <klajdbudlla@klajds-mbp.zte.com.cn>
2019-05-15 21:55:33 +0200

    Updates

---
+++ precache-manifest.02dc6352355db82d6d8e3f60b428447b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d61db942ad6cf3e48e13d8955588eb3",
+    "url": "/deploy-lastfmclone/index.html"
+  },
+  {
+    "revision": "21c6c3335e7263d4c624",
+    "url": "/deploy-lastfmclone/static/css/2.87619ade.chunk.css"
+  },
+  {
+    "revision": "477d80a800f592ba85ba",
+    "url": "/deploy-lastfmclone/static/css/main.01c76a8d.chunk.css"
+  },
+  {
+    "revision": "21c6c3335e7263d4c624",
+    "url": "/deploy-lastfmclone/static/js/2.87d546af.chunk.js"
+  },
+  {
+    "revision": "477d80a800f592ba85ba",
+    "url": "/deploy-lastfmclone/static/js/main.6752a361.chunk.js"
+  },
+  {
+    "revision": "89492b4baf53abe3c8d0",
+    "url": "/deploy-lastfmclone/static/js/runtime~main.94dfe1a7.js"
+  }
+]);
