4c4c5f468c6fafa14f98d26d9293290716aee9ef
Klajd Budlla <klajdbudlla@klajds-mbp.zte.com.cn>
2019-05-17 01:13:58 +0200

    Updates

---
+++ precache-manifest.8e3b007bc9b16abcc026e6a64721c045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58d74ee14b1b9ccc2eb72678e83a53c",
+    "url": "/deploy-lastfmclone/index.html"
+  },
+  {
+    "revision": "fb44b6232f102a349004",
+    "url": "/deploy-lastfmclone/static/css/2.3667796c.chunk.css"
+  },
+  {
+    "revision": "4faccf121812636c413d",
+    "url": "/deploy-lastfmclone/static/css/main.06597038.chunk.css"
+  },
+  {
+    "revision": "fb44b6232f102a349004",
+    "url": "/deploy-lastfmclone/static/js/2.9ea2c700.chunk.js"
+  },
+  {
+    "revision": "4faccf121812636c413d",
+    "url": "/deploy-lastfmclone/static/js/main.fe75887b.chunk.js"
+  },
+  {
+    "revision": "89492b4baf53abe3c8d0",
+    "url": "/deploy-lastfmclone/static/js/runtime~main.94dfe1a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/deploy-lastfmclone/precache-manifest.d61428d5815093c38331ada89987f342.js"
+  "/deploy-lastfmclone/precache-manifest.8e3b007bc9b16abcc026e6a64721c045.js"
 );
 
 self.addEventListener('message', (event) => {
